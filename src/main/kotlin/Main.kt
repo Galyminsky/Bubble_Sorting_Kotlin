285acfbfa3da8450ea086e4b4b74312efdebf742
Galyminsky-PC <jobforandoid@protonmail.com>
2022-05-23 21:17:18 +0600

    Пузырьковая Сортировка на убывание, Котлин

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+
+
+    fun main() {
+        val sort: (Array<Int>) -> Array<Int> = {
+            for (i in it.size - 2 downTo 0) {
+                for (j in 0..i) {
+                    if (it[j] < it[j + 1]) {
+                        val temp = it[j]
+                        it[j] = it[j + 1]
+                        it[j + 1] = temp
+                    }
+                }
+            }
+            it
+        }
+        val sortedArray = sort(arrayOf(5,-7,6,4,-9,-1,7,5,-3))
+        for (i in sortedArray) {
+            println(i)
+        }
+    }
+
